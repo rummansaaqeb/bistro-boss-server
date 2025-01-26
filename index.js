ab517878f2a98e544d72057f8cbf78eca4eaeecd
rummansaaqeb <rsaaqeb@gmail.com>
2025-01-26 22:29:10 +0600

    added userCollection in the database and added a post request for users

--- index.js
+++ index.js
@@ -30,8 +30,18 @@ async function run() {
 
 
         const menuCollection = client.db("bistroDB").collection('menu');
+        const userCollection = client.db("bistroDB").collection('users');
         const reviewCollection = client.db("bistroDB").collection('reviews');
         const cartCollection = client.db("bistroDB").collection('carts');
+
+
+        // user related api
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            res.send(result)
+        })
 
         app.get('/menu', async (req, res) => {
             const cursor = menuCollection.find();
@@ -61,7 +71,7 @@ async function run() {
 
         app.delete('/carts/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await cartCollection.deleteOne(query);
             res.send(result);
         })
