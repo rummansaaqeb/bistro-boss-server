e617492153f7ece6813e7b72427ac2af97e38879
rummansaaqeb <rsaaqeb@gmail.com>
2025-01-27 11:01:31 +0600

    added a delete request for dashboard

--- index.js
+++ index.js
@@ -57,6 +57,14 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // menu related apis
         app.get('/menu', async (req, res) => {
             const cursor = menuCollection.find();
             const result = await cursor.toArray();
