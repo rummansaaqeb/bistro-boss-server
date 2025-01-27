f44d66eecb864b5014b25988c4c234d911e7b9cc
rummansaaqeb <rsaaqeb@gmail.com>
2025-01-27 19:59:01 +0600

    Displayed card error and created server side payment intents

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-require('dotenv').config()
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // middlewares
@@ -159,7 +160,7 @@ async function run() {
         app.patch('/menu/:id', async (req, res) => {
             const item = req.body;
             const id = req.params.id;
-            const filter = {_id: id};
+            const filter = { _id: id };
             const updatedDoc = {
                 $set: {
                     name: item.name,
@@ -198,8 +199,21 @@ async function run() {
             const query = { _id: new ObjectId(id) };
             const result = await cartCollection.deleteOne(query);
             res.send(result);
-        })
+        });
 
+        // payment intent
+        app.post('/create-payment-intent', async (req, res) => {
+            const { price } = req.body;
+            const amount = parseInt(price * 100);
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            })
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
