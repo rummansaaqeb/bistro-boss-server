d64db47f0ea191f489c92e86349dbc34d3ed77f1
rummansaaqeb <rsaaqeb@gmail.com>
2025-01-27 20:12:31 +0600

    sent price and got client secret from stripe

--- index.js
+++ index.js
@@ -205,6 +205,7 @@ async function run() {
         app.post('/create-payment-intent', async (req, res) => {
             const { price } = req.body;
             const amount = parseInt(price * 100);
+            console.log(amount, 'amount inside the intent');
             const paymentIntent = await stripe.paymentIntents.create({
                 amount: amount,
                 currency: 'usd',
